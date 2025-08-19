d75ea44893c53e12dd0cdd5dd0012395e0c06f08
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-08-19 20:19:02 +0300

    Added Product Services and API with filters

--- core/apps/products/admin.py
+++ core/apps/products/admin.py
@@ -6,4 +6,10 @@ from core.apps.products.models.products import Product
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "created_at", "updated_at")
+    list_display = (
+        "id",
+        "title",
+        "created_at",
+        "updated_at",
+        "is_visible",
+    )
